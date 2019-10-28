dce128a490231f8626a068bb807d889cb4457960
Samir Allous <samirxallous@gmail.com>
2019-10-28 20:12:23 +0100

    Dockerize the app

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ class ProductionConfig:
     DEBUG = False
     TESTING = False
     SECRET_KEY = '1nlGIl5nNlRKiat3QtK7'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
